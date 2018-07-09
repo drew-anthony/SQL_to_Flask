d2d7a55358fe025130d61a0d2d6e88dd4115db6d
drew-anthony <39780586+drew-anthony@users.noreply.github.com>
2018-07-09 11:55:49 -0500

    Add files via upload
    MySQL connection errors assignment

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+# import the function connectToMySQL from the file mysqlconnection.py
+from mysqlconnection import connectToMySQL
+app = Flask(__name__)
+# invoke the connectToMySQL function and pass it the name of the database we're using
+# connectToMySQL returns an instance of MySQLConnection, which we will store in the variable 'mysql'
+mysql = connectToMySQL('sakila')
+# now, we may invoke the query_db method
+print("all the users", mysql.query_db("SELECT * FROM category;"))
+app.run(debug=True)
